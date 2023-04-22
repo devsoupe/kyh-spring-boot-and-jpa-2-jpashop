aca47cacb95dceaf894076b5a12d02b5e0b375ce
devsoupe <luvyum4e@gmail.com>
2023-04-22 20:08:44 +0900

    프로젝트 생성

---
+++ src/main/java/jpabook/jpashop/KyhSpringBootAndJpa1JpashopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhSpringBootAndJpa1JpashopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyhSpringBootAndJpa1JpashopApplication.class, args);
+    }
+
+}
