dedb0d5e30cdc8322b56dfea297f4d0870c8d822
devsoupe <luvyum4e@gmail.com>
2023-05-06 20:54:44 +0900

    주문 서비스 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -11,7 +11,7 @@ public class OrderRepository {
 
     private final EntityManager em;
 
-    private void save(Order order) {
+    public void save(Order order) {
         em.persist(order);
     }
 
