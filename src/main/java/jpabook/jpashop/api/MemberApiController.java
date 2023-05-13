897dd013467339817ab2fa0a601592e9352051f9
devsoupe <luvyum4e@gmail.com>
2023-05-13 22:35:52 +0900

    회원 등록 API - V1

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class CreateMemberResponse {
+
+        private Long id;
+    }
+}
